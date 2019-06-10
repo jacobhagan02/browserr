0e7dc1cb4c9b108ad6896ba3dd4b3633e55c07a4
eatmyvenom <jacobhagan@outlook.com>
2019-06-10 10:17:33 -0700

    clipboard api done

--- scripts/mozExt/clipboard.js
+++ scripts/mozExt/clipboard.js
@@ -4,12 +4,16 @@ const buffer = require('buffer');
 module.exports = class{
     static setImageData(imageData, imageType){
         return new Promise((resolve,reject)=>{
-            var bfr = buffer.from(imageData);
-            var img = nativeImage.createFromBuffer(bfr);
-            var image = (imageType == 'png') ? img.toPNG({scaleFactor : 1.0}) : img.toJPEG(100);
+            try{
+                var bfr = buffer.from(imageData);
+                var img = nativeImage.createFromBuffer(bfr);
+                var image = (imageType == 'png') ? img.toPNG({scaleFactor : 1.0}) : img.toJPEG(100);
 
-            require('electron').remote.clipboard.writeImage(image);
-            resolve();
+                require('electron').remote.clipboard.writeImage(image);
+                resolve();
+            }catch(e){
+                reject(e);
+            }
         });
     }
 }

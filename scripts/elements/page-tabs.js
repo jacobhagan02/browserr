63144f99d3c5953cf44919da1f6e6e5b4bb0b5e7
eatmyvenom <jacobhagan2002@gmail.com>
2019-06-16 14:27:49 -0700

    fullscreen fix

--- scripts/elements/page-tabs.js
+++ scripts/elements/page-tabs.js
@@ -1,3 +1,11 @@
+require('electron').remote.getCurrentWindow().on('maximize',()=>{
+    document.querySelector('page-tabs').style.paddingLeft = '';
+});
+
+require('electron').remote.getCurrentWindow().on('unmaximize',()=>{
+    document.querySelector('page-tabs').style.paddingLeft = '70px'
+});
+
 module.exports = class extends HTMLElement {    
     constructor(){
         super();
@@ -7,7 +15,6 @@ module.exports = class extends HTMLElement {
     connectedCallback(){
         if(process.platform === 'darwin'){
             this.style.paddingLeft = '70px';
-            
         }
     }
 }

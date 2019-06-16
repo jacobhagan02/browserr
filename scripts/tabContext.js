7c0254eefa84d7d3902d15a18e4ec1c0dc5d9626
eatmyvenom <jacobhagan2002@gmail.com>
2019-06-16 13:52:53 -0700

    menu test

--- scripts/tabContext.js
+++ scripts/tabContext.js
@@ -50,13 +50,98 @@ function newWindow(){
     win.loadFile(__dirname+'/index.html');
 }
 
-let mnu = Menu.buildFromTemplate([
-    {label:'Close Tab',accelerator : 'Ctrl+W',click:closeTab},
-    {label:'Dev Tools',accelerator:'Ctrl+Shift+I',click:openDevTools},
-    {label:'New Tab',accelerator:'Ctrl+T',click:newTab},
-    {label:'New Window',accelerator:'Ctrl+N',click:newWindow},
-    {label:'Find In Page',accelerator:'Ctrl+F',click:findTxt}
-]);
+let mnu = Menu.buildFromTemplate(
+        [
+        // { role: 'appMenu' }
+        ...(process.platform === 'darwin' ? [{
+          label: app.getName(),
+          submenu: [
+            { role: 'about' },
+            { type: 'separator' },
+            { role: 'services' },
+            { type: 'separator' },
+            { role: 'hide' },
+            { role: 'hideothers' },
+            { role: 'unhide' },
+            { type: 'separator' },
+            { role: 'quit' }
+          ]
+        }] : []),
+        // { role: 'fileMenu' }
+        {
+          label: 'File',
+          submenu: [
+            isMac ? { role: 'close' } : { role: 'quit' }
+          ]
+        },
+        // { role: 'editMenu' }
+        {
+          label: 'Edit',
+          submenu: [
+            { role: 'undo' },
+            { role: 'redo' },
+            { type: 'separator' },
+            { role: 'cut' },
+            { role: 'copy' },
+            { role: 'paste' },
+            ...(isMac ? [
+              { role: 'pasteAndMatchStyle' },
+              { role: 'delete' },
+              { role: 'selectAll' },
+              { type: 'separator' },
+              {
+                label: 'Speech',
+                submenu: [
+                  { role: 'startspeaking' },
+                  { role: 'stopspeaking' }
+                ]
+              }
+            ] : [
+              { role: 'delete' },
+              { type: 'separator' },
+              { role: 'selectAll' }
+            ])
+          ]
+        },
+        // { role: 'viewMenu' }
+        {
+          label: 'View',
+          submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { role: 'toggledevtools' },
+            { type: 'separator' },
+            { role: 'resetzoom' },
+            { role: 'zoomin' },
+            { role: 'zoomout' },
+            { type: 'separator' },
+            { role: 'togglefullscreen' }
+          ]
+        },
+        // { role: 'windowMenu' }
+        {
+          label: 'Window',
+          submenu: [
+            { role: 'minimize' },
+            { role: 'zoom' },
+            ...(isMac ? [
+              { type: 'separator' },
+              { role: 'front' },
+              { type: 'separator' },
+              { role: 'window' }
+            ] : [
+              { role: 'close' }
+            ])
+          ]
+        },
+    
+    {label:'Current Tab', submenu: [
+        {label:'Close Tab',accelerator : 'Ctrl+W',click:closeTab},
+        {label:'Dev Tools',accelerator:'Ctrl+Shift+I',click:openDevTools},
+        {label:'New Tab',accelerator:'Ctrl+T',click:newTab},
+        {label:'New Window',accelerator:'Ctrl+N',click:newWindow},
+        {label:'Find In Page',accelerator:'Ctrl+F',click:findTxt}
+]}]);
 
 Menu.setApplicationMenu(mnu);
 
